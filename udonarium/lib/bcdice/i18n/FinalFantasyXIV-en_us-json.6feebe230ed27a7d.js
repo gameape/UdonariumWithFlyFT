7d47ece90706e41551a716ad5d93c1e5307830db
gameape <17588263+gameape@users.noreply.github.com>
2025-04-21 14:05:24 +0900

    地形を点滅させない版に更新

--- udonarium/lib/bcdice/i18n/FinalFantasyXIV-en_us-json.6feebe230ed27a7d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkudonarium=self.webpackChunkudonarium||[]).push([[9250],{3703:e=>{e.exports=JSON.parse('{"en_us":{"FinalFantasyXIV":{"directhit":"Direct Hit","normalhit":"Only Base Effect"}}}')}}]);
